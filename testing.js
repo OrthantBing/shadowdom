d04b9d1f9167ed2ae16946e008237df708eef8ad
Anton Bright Thomson <antonbright@athenahealth.com>
2017-11-29 15:59:05 +0530

    initial commit

---
+++ testing.js
@@ -0,0 +1,84 @@
+var metadata = {
+        'id': arguments[0],
+        'className': arguments[1],
+        'tagName': arguments[2]
+};
+window.test = function(m){
+    var ShadowRoots = function(dom) {
+        this.rootdom = dom;
+        this.shadowRoots;
+        this.shadowRootParents;
+        this.allShadowRoots(dom);
+        
+    };
+
+    ShadowRoots.prototype = function(){
+        var _getNodeListUsingIterator = function(dom){
+            let nodeIterator = document.createNodeIterator( dom, NodeFilter.SHOW_ALL, function(node){ 
+                if (node.shadowRoot) {
+                    return true;
+                } 
+            });
+            var nodelist = [];
+            var currentNode;
+            while (currentNode = nodeIterator.nextNode()){
+                nodelist.push(currentNode);
+            }
+            return nodelist;
+        }
+
+        return {
+            allShadowRoots: function(dom){
+                var nodelist = _getNodeListUsingIterator(document.body);
+                for (var i = 0; i < nodelist.length; i+=1){
+                    var insideShadow = nodelist[i].shadowRoot;
+                    Array.prototype.push.apply(nodelist, _getNodeListUsingIterator(insideShadow))
+                }
+                this.shadowRootParents = nodelist;
+                var returnnodes = nodelist.map(function(node){
+                    return node.shadowRoot;
+                })
+                this.shadowRoots = returnnodes;
+                return returnnodes;
+            },
+            getShadowRootUsingMetadata: function(metadata) {
+                /* metadatainfo will be of the form
+                 * {
+                 *  id: '',
+                 *  tagName: '',
+                 *  className: '',
+                 * }
+                 * 
+                 */
+                 var metadatanames = Object.getOwnPropertyNames(metadata);
+                 metadatanames = metadatanames.filter(function(val){
+                     if(metadata[val]){
+                         return true;
+                     }
+                 });
+
+                 var returnvalue = this.shadowRootParents.filter(function(node){
+
+                     var bool = metadatanames.every(function(val){
+
+                        return node[val] === metadata[val];
+                     });
+                     return bool;
+                 });
+
+                 var shadowRootValues = returnvalue.map(function(val){
+                     return val.shadowRoot;
+                 });
+                 return shadowRootValues;
+            }
+        }
+    }();
+
+    var shadowrootobj = new ShadowRoots(document.body);
+    
+    return shadowrootobj.getShadowRootUsingMetadata(metadata);
+
+};
+return window.test(metadata);
+
+
